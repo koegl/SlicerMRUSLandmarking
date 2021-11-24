82f0d80c8eba55f3141ff620e606c4a1b46f9cbd
Fryderyk <koeglfryderyk@gmail.com>
2021-11-24 18:56:33 +0100

    added slider functionality - do sth when it changes

--- opacity_ui/main.py
+++ opacity_ui/main.py
@@ -1,5 +1,6 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow
+import numpy as np
+from PySide6.QtWidgets import QApplication, QMainWindow, QSlider
 from opacity_ui import Ui_MainWindow
 
 
@@ -9,8 +10,19 @@ class OpacityWindow(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
-    def print_slider_value(self):
-        print(self.ui.verticalSlider.value())
+        # slider
+        self.ui.verticalSlider.setMinimum(0)
+        self.ui.verticalSlider.setMaximum(1000)
+        self.ui.verticalSlider.setTickInterval(100)
+        self.ui.verticalSlider.setTickPosition(QSlider.TicksLeft)
+
+        # whenever the slider changes, execute the opacity change
+        self.ui.verticalSlider.valueChanged.connect(self.opacity_change)
+
+    def opacity_change(self):
+        x = self.ui.verticalSlider.value()
+        print("{}^2 = {}".format(x, x**2))
+        x = 0
 
 
 if __name__ == "__main__":
