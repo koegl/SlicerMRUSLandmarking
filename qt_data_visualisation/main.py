9f15f861262f54c088ba624a57c5b8dd169e9d0e
Fryderyk <koeglfryderyk@gmail.com>
2021-11-25 19:30:19 +0100

    rearranged loading function and added documentation

--- qt_data_visualisation/main.py
+++ qt_data_visualisation/main.py
@@ -2,18 +2,27 @@ import argparse
 import pandas as pd
 import os
 
+from PySide6.QtCore import QDateTime, QTimeZone
+
 
 def read_data(file_path):
-    return pd.read_csv(file_path)
+    """
+    Read data from a csv file
+    :param file_path: The path tot he file
+    :return: loaded csv data
+    """
+    if file_path is None:
+        path = os.path.join(os.getcwd(), "..", "data", "all_day.csv")
+        data = pd.read_csv(path)
+    else:
+        data = pd.read_csv(file_path)
+
+    return data
 
 
 def main(args):
 
-    if args.file_path is None:
-        path = os.path.join(os.getcwd(), "..", "data", "all_day.csv")
-        data = read_data(path)
-    else:
-        data = read_data(args.file_path)
+    print(read_data(args.file_path))
 
 
 if __name__ == "__main__":
